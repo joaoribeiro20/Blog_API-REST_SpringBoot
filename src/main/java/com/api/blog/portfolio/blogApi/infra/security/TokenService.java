e1648b485979b4b1e30658005b4807a6bfa81781
Joao Ribeiro <jvalves217@gmail.com>
2024-05-18 10:27:50 -0300

    feat: Create Class TokenService

--- src/main/java/com/api/blog/portfolio/blogApi/infra/security/TokenService.java
+++ src/main/java/com/api/blog/portfolio/blogApi/infra/security/TokenService.java
@@ -1,9 +1,15 @@
 package com.api.blog.portfolio.blogApi.infra.security;
 
 import com.api.blog.portfolio.blogApi.entities.User;
+import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 @Service
 public class TokenService {
@@ -13,11 +19,34 @@ public class TokenService {
     public String generateToken(User user) {
         try {
             Algorithm algorithm = Algorithm.HMAC256(secret);
-
-        } catch () {
-
+            String token = JWT.create()
+                    .withIssuer("auth-api-blog")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Erro ao gerar o token", exception);
         }
-
     }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(6).toInstant(ZoneOffset.of("-03:00"));
+    }
+
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api-blog")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Token invalido ou expirado", exception);
+        }
+    }
+
 
 }
