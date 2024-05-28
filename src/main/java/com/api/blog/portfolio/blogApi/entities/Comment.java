e72a16624c76ecdde77e06b571ba27e87e268ae8
Joao Ribeiro <jvalves217@gmail.com>
2024-05-28 10:37:37 -0300

    refactor: attribute removal subject

--- src/main/java/com/api/blog/portfolio/blogApi/entities/Comment.java
+++ src/main/java/com/api/blog/portfolio/blogApi/entities/Comment.java
@@ -1,9 +1,11 @@
 package com.api.blog.portfolio.blogApi.entities;
 
+import com.api.blog.portfolio.blogApi.entities.user.User;
 import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity(name = "comments")
 @Table(name = "comments")
@@ -19,13 +21,10 @@ public class Comment {
     private String id;
     @Column(nullable = false)
     private String content;
-    @Column(nullable = false)
-    //a proriedade a baixo pode ser um enum     @Enumerated(EnumType.STRING)
-    private String subject;
     @Column(name = "created_at")
     private LocalDateTime createdAt;
-    @Column(name = "update_at")
-    private LocalDateTime updateAt;
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
     @ManyToOne()
     @JoinColumn(name = "user_id")
     private User user;
